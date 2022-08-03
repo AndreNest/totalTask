3b0276340c319c28398071ddf9a1b077b2203a33
AndreyNesterov <1@nestandre.ru>
2022-08-04 02:16:27 +0300

    add Array

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// программа из имеющегося массива строк делает массив формирует массив из строк с тремя и менее символами
+Console.WriteLine("Сколько будет элементов в исходном массиве?");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[n];
+for(int i = 0; i < n; i++)
+{
+    array[i] = Console.ReadLine();
+}
+Console.WriteLine();
+Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
