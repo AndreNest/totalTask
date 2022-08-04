97ceaafeb502bc19e640595fa8b7f2212dccd76c
AndreyNesterov <1@nestandre.ru>
2022-08-04 20:43:48 +0300

    version 1.0

--- Program.cs
+++ Program.cs
@@ -1,10 +1,37 @@
 ﻿// программа из имеющегося массива строк делает массив формирует массив из строк с тремя и менее символами
-Console.WriteLine("Сколько будет элементов в исходном массиве?");
+Console.WriteLine("сколько значений будет в массиве?");
 int n = Convert.ToInt32(Console.ReadLine());
 string[] array = new string[n];
-for(int i = 0; i < n; i++)
+string[] newArray = new string[array.Length];
+for (int i = 0; i < n; i++)
 {
+    Console.Write($"Введите значение №{i + 1}: ");
     array[i] = Console.ReadLine();
 }
-Console.WriteLine();
-Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+void SelectArray(string[] array, string[] newArray)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4)
+        {
+            newArray[count] = array[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + "\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Исходный массив: ");
+PrintArray(array);
+SelectArray(array, newArray);
+Console.WriteLine("Полученный массив: ");
+PrintArray(newArray);
+
+
